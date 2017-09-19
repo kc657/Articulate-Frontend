59e6dbaa5e93570eb9b000c21b7d589cac4269a4
kc657 <kvn.0218@gmail.com>
2017-09-19 12:58:38 -0700

    added more instructions

--- src/components/Body/ProfilePage.js
+++ src/components/Body/ProfilePage.js
@@ -13,9 +13,9 @@ class ProfilePage extends Component {
           {(this.props.selectedProject) ? <AttemptList selectedProjectTone={this.props.selectedProjectTone} clickNewAttempt={this.props.clickNewAttempt} handleProjectDelete={this.props.handleProjectDelete} selectedProject={this.props.selectedProject} selectedProjectTitle={this.props.selectedProjectTitle} currentUserId={this.props.currentUserId} /> :
           <div className='instructions center middle'>
             <h2>Instructions</h2>
+            <h5>Welcome to Articulat.in! This application scores the differential between a speech transcript with your voice input. By doing so, we hope to highlight places you can improve on for your upcoming speech. The app also uses a tone-analyzer to give you some additional fun stats regarding your transcript and recording.</h5>
             <ol>
               <li>Create a new project on the left-hand side with a transcript you would like to practice on</li>
-              <li>In the list of personal projects, click and select the project you would like to work on</li>
               <li>Select 'Add New Recording' and submit your recording after you have completed it for grading</li>
               <li>Use the scoring to make any adjustments to your speech before the big event!</li>
             </ol>
