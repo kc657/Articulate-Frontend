986f1297e1b7ed5122fd1c4f0818cc5ade4b8b72
kc657 <kvn.0218@gmail.com>
2017-09-20 13:45:21 -0700

    refactored signup/signin modal to fix mobile issue

--- src/components/Body/GradePage.js
+++ src/components/Body/GradePage.js
@@ -4,7 +4,7 @@ class GradePage extends Component {
   render () {
     return (
       <div className='GradePage center'>
-        <h1 className='center'> Yay, your recording has been saved and graded! Press the button below to go back to your profile page to see the score sheet. </h1>
+        <h2 className='center'> Yay, your recording has been saved and graded! Press the button below to go back to your profile page to see the score sheet. </h2>
         <a className='waves-effect waves-dark btn center' onClick={this.props.clickNewAttempt}>Save and Go Home</a>
       </div>
     )
